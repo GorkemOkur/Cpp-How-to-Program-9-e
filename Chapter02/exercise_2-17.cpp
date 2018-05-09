9d122afd7119088352f21efe218d72cc83a30b00
GorkemOkur <gorkemokur@gmail.com>
2018-05-09 09:54:23 +0200

    exercise_2-17 (Printing)

---
+++ Chapter02/exercise_2-17.cpp
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  exercise_2-17.cpp
+ *
+ *    Description: (Printing): Write a program that prints  the numbers 1 to  4 on the same line with each pair of adjacent numbers separated by one space. Do this several ways:
+ *					a) Using one statement with one stream insertion operator.
+ *					b) Using one statement with four stream insertion operators.
+ *					c) Using four statements.
+ *
+ *        Version:  1.0
+ *        Created:  09/05/18 09:44
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Şükrü Görkem Okur - gorkemokur@gmail.com
+ *   Organization:  -
+ *
+ *   One person can make adifference and every personshouldtry.
+ *	 —John F. Kennedy
+ * =====================================================================================
+ *
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+cout<<"1 2 3 4\n";
+
+cout<<"1 2"<<" "<<"3 4"<<endl;
+
+cout<<"1 ";
+cout<<"2 ";
+cout<<"3 ";
+cout<<"4";
+
+return 0;
+}
