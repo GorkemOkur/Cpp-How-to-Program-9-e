fefcda425056e562f6c21809ad7f015eb90bea83
GorkemOkur <gorkemokur@gmail.com>
2018-05-11 01:32:25 +0200

    exercise_3-11 (Modifying Class GradeBook)

--- Chapter03/exercise_3-11.cpp
+++ Chapter03/exercise_3-11.cpp
@@ -4,11 +4,11 @@
  *       Filename:  exercise_3-11.cpp
  *
  *     Description: 3.11 (Modifying Class GradeBook)
- *								  a) Include a second string data member that represents the course instructor’s name
- *								  b) Provide a set function to change the instructor’s name and a get function to retrieve it.
- *								  c) Modify the constructor to specify course name and instructor name parameters.
- *								  d) Modify function displayMessage to output the welcome message and course name,
- *									  then the string "This course is presented by: " followed by the instructor’s name.
+ *			  a) Include a second string data member that represents the course instructor’s name
+ *			  b) Provide a set function to change the instructor’s name and a get function to retrieve it.
+ *			  c) Modify the constructor to specify course name and instructor name parameters.
+ *			  d) Modify function displayMessage to output the welcome message and course name,
+ *			     then the string "This course is presented by: " followed by the instructor’s name.
  *	
  *
  *        Version:  1.0
