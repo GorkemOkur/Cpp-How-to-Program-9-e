0efdb9bad563b142a375b7c14bd9db8caf25f48c
GorkemOkur <gorkemokur@gmail.com>
2018-05-13 01:19:18 +0200

    exercise_4-29 (Multiples of 2 with an Infinite Loop)

---
+++ Chapter04/exercise_4-29.cpp
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  exercise_4-29.cpp
+ *
+ *     Description: 4.29 (Multiples of 2 with an Infinite Loop)
+ *								  
+ *
+ *        Version:  1.0
+ *        Created:  13/05/18 00:50:00
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Şükrü Görkem Okur - gorkemokur@gmail.com
+ *  	 Organization:  -
+ *
+ *   All the evolution we know of proceeds from the vague to the definite.
+ *   —Charles Sanders Peirce
+ * =====================================================================================
+ */
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int col=1;
+
+	while(true){
+		col=col*2;
+		cout<<col;
+	}
+
+//write infinite 000000000000000
+
+return 0;
+}
