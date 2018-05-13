d940b1a9993a9965b7870d5a32b8d3dd63d29c2c
GorkemOkur <gorkemokur@gmail.com>
2018-05-13 22:31:05 +0200

    exercise_5-20 (Number Systems Table)

---
+++ Chapter05/exercise_5-20.cpp
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  exercise_5-20.cpp
+ *
+ *     Description: 5.20 (Number Systems Table)
+ *								  
+ *
+ *        Version:  1.0
+ *        Created:  13/05/18 16:10:00
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Şükrü Görkem Okur - gorkemokur@gmail.com
+ *  	 Organization:  -
+ *
+ *   Who can control his fate?
+ *   —William Shakespeare
+ * =====================================================================================
+ */
+
+#include<iostream>
+#include<cmath>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+	
+	for (int i = 3; i < 500; ++i)
+	{
+		for (int j = 4; j <500 ; ++j)
+		{
+			for (int k = 5; k <500 ; ++k)
+			{
+				if( (pow(i,2)+pow(j,2)) == pow(k,2) ){
+					cout<<"valid ->  i: "<<i<<" j: "<<j<<" k: "<<k<<endl;
+				}
+			}
+		}
+	}
+
+
+return 0;
+}
